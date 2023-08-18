b49612d5aa5f259792bdfd5dfe98e6cc9c5514c1
taeho <goekddkdlel@naver.com>
2023-08-18 15:17:41 +0900

    first commit 08.18

---
+++ src/main/java/com/teami/banham/BanhamApplication.java
@@ -0,0 +1,13 @@
+package com.teami.banham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanhamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanhamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teami/banham/BanhamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teami.banham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanhamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
